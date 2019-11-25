a5bf22408c04068da52723a5d3a591f3306ff07c
rafee <s2N@J9^J9b%C>
2019-11-24 23:10:05 -0500

    Push 24 Nov Night Version to git

--- app/src/main/java/com/google/android/gms/fit/samples/stepcounter/MainActivity.java
+++ app/src/main/java/com/google/android/gms/fit/samples/stepcounter/MainActivity.java
@@ -15,45 +15,42 @@
  */
 package com.google.android.gms.fit.samples.stepcounter;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.fit.samples.common.logger.Log;
-import com.google.android.gms.fit.samples.common.logger.LogView;
 import com.google.android.gms.fit.samples.common.logger.LogWrapper;
 import com.google.android.gms.fit.samples.common.logger.MessageOnlyLogFilter;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
-import com.google.android.gms.fitness.SessionsApi;
-import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.data.Session;
 import com.google.android.gms.fitness.request.SessionReadRequest;
 import com.google.android.gms.fitness.result.SessionReadResponse;
-import com.google.android.gms.fitness.result.SessionReadResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -63,13 +60,14 @@ import java.util.concurrent.TimeUnit;
 
 import static java.text.DateFormat.getDateInstance;
 
+
 /**
  * This sample demonstrates combining the Recording API and History API of the Google Fit platform
  * to record steps, and display the daily current step count. It also demonstrates how to
  * authenticate a user with Google Play Services.
  */
 public class MainActivity extends AppCompatActivity {
-//    protected LocationManager locationManager;
+    //    protected LocationManager locationManager;
 //    protected LocationListener locationListener;
 //    protected Context context;
 //    TextView txtLat;
@@ -87,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
         // This method sets up our custom logger, which will print all log messages to the device
         // screen, as well as to adb logcat.
         initializeLogging();
+//        TextView sleepBox = (TextView) findViewById(R.id.sleep);
 
 //        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 //        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -108,7 +107,6 @@ public class MainActivity extends AppCompatActivity {
                         .addDataType(DataType.TYPE_ACTIVITY_SEGMENT)
                         .build();
 
-
         if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
             GoogleSignIn.requestPermissions(
                     this,
@@ -117,8 +115,21 @@ public class MainActivity extends AppCompatActivity {
                     fitnessOptions);
         } else {
             subscribe();
-            readSession();
+            long sleepTime = readSession();
+            sendRequest("5dcb7f0072c6150d68c07a59", 16599, (float) 7.5);
         }
+
+        Button sync = findViewById(R.id.sync);
+        sync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long sleepTime = readSession();
+                TextView sleepBox = findViewById(R.id.sleep);
+                sleepBox.setText(String.valueOf(sleepTime));
+                readData();
+//                sendRequest();
+            }
+        });
     }
 
     @Override
@@ -151,14 +162,15 @@ public class MainActivity extends AppCompatActivity {
                         });
     }
 
-    private Task<SessionReadResponse> readSession() {
+    private long readSession() {
         // Begin by creating the query.
         Calendar cal = Calendar.getInstance();
         Date now = new Date();
         cal.setTime(now);
         long endTime = cal.getTimeInMillis();
-        cal.add(Calendar.DAY_OF_WEEK_IN_MONTH, -4);
+        cal.add(Calendar.DAY_OF_MONTH, -1);
         long startTime = cal.getTimeInMillis();
+        final long[] sleepTime = new long[1];
 
         final java.text.DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Log.i(TAG, "Range Start: " + dateFormat.format(startTime));
@@ -171,13 +183,7 @@ public class MainActivity extends AppCompatActivity {
                 .enableServerQueries();
 
         final SessionReadRequest readRequest = sessionBuilder.build();
-//        SessionReadRequest readRequest = readFitnessSession();
-
-        // [START read_session]
-        // Invoke the Sessions API to fetch the session with the query and wait for the result
-        // of the read request. Note: Fitness.SessionsApi.readSession() requires the
-        // ACCESS_FINE_LOCATION permission.
-        return Fitness.getSessionsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+        Fitness.getSessionsClient(this, GoogleSignIn.getLastSignedInAccount(this))
                 .readSession(readRequest)
                 .addOnSuccessListener(new OnSuccessListener<SessionReadResponse>() {
                     @Override
@@ -189,22 +195,16 @@ public class MainActivity extends AppCompatActivity {
 
                         for (Session session : sessions) {
                             // Process the session
-//                            Log.i(TAG, session.getName().equals("Sleep"));
                             if (session.getName().equals("Sleep")) {
 //                                long startTime = ;
                                 Log.i(TAG, "Time to start sleep" + dateFormat.format(session.getStartTime(TimeUnit.MILLISECONDS)));
                                 Log.i(TAG, "Time to end sleep" + dateFormat.format(session.getEndTime(TimeUnit.MILLISECONDS)));
 //                                Log.i(TAG, session.toString());
+                                sleepTime[0] = session.getEndTime(TimeUnit.MINUTES) - session.getStartTime(TimeUnit.MINUTES);
+//                                TextView sleepBox = findViewById(R.id.sleep);
+//                                sleepBox.setText(String.valueOf(sleepTime[0]));
+                                Log.i(TAG, "Total Sleep Time" + String.valueOf(sleepTime[0]) + "Minutes");
                             }
-
-                            // Process the data sets for this session
-//                            List<DataSet> dataSets = sessionReadResponse.getDataSet(session);
-//                            for (DataSet dataSet : dataSets) {
-//                                for (DataPoint dataPoint : dataSet.getDataPoints()) {
-//
-//                                }
-////                                dumpDataSet(dataSet);
-//                            }
                         }
                     }
                 })
@@ -214,7 +214,69 @@ public class MainActivity extends AppCompatActivity {
                         Log.i(TAG, "Failed to read session");
                     }
                 });
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return sleepTime[0];
         // [END read_session]
+    }
+
+    private void sendRequest(String id, long steps, float sleepingHours) {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://c7trbjve0a.execute-api.us-east-1.amazonaws.com/v1/datas/steps";
+//        String url ="http://www.mohammadrafee.com";
+
+        final JSONObject jsonObject = new JSONObject();
+        try {
+            long today = System.currentTimeMillis();
+            jsonObject.put("id", id);
+            jsonObject.put("date", today);
+            jsonObject.put("steps", steps);
+            jsonObject.put("sleepingHours", sleepingHours);
+        } catch (JSONException e) {
+            // handle exception
+        }
+        JsonObjectRequest putRequest;
+        putRequest = new JsonObjectRequest(Request.Method.PUT, url, jsonObject,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // response
+                        Log.i(TAG, response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.w(TAG, error.toString());
+                    }
+                }
+        );
+
+//        JsonArrayRequest putRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+//                new Response.Listener<JSONArray>()
+//                {
+//                    @Override
+//                    public void onResponse(JSONArray response) {
+//                        // display response
+//                        Log.i(TAG, response.toString());
+//                    }
+//                },
+//                new Response.ErrorListener()
+//                {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.w(TAG, error.toString());
+//                    }
+//                }
+//        );
+
+// Add the request to the RequestQueue.
+        queue.add(putRequest);
     }
 
     /**
@@ -222,6 +284,7 @@ public class MainActivity extends AppCompatActivity {
      * current timezone.
      */
     private void readData() {
+        final long[] steps = new long[1];
         Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
                 .readDailyTotal(DataType.TYPE_STEP_COUNT_DELTA)
                 .addOnSuccessListener(
@@ -232,7 +295,10 @@ public class MainActivity extends AppCompatActivity {
                                         dataSet.isEmpty()
                                                 ? 0
                                                 : dataSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+//                                steps[0] = total;
                                 Log.i(TAG, "Total no of steps: " + total);
+                                TextView stepsBox = (TextView) findViewById(R.id.steps);
+                                stepsBox.setText(String.valueOf(total));
                             }
                         })
                 .addOnFailureListener(
@@ -273,14 +339,14 @@ public class MainActivity extends AppCompatActivity {
         MessageOnlyLogFilter msgFilter = new MessageOnlyLogFilter();
         logWrapper.setNext(msgFilter);
         // On screen logging via a customized TextView.
-        LogView logView = (LogView) findViewById(R.id.sample_logview);
+//        LogView logView = (LogView) findViewById(R.id.sample_logview);
 
         // Fixing this lint error adds logic without benefit.
         // noinspection AndroidLintDeprecation
-        logView.setTextAppearance(R.style.Log);
+//        logView.setTextAppearance(R.style.Log);
 
-        logView.setBackgroundColor(Color.WHITE);
-        msgFilter.setNext(logView);
+//        logView.setBackgroundColor(Color.WHITE);
+//        msgFilter.setNext(logView);
         Log.i(TAG, "Ready");
     }
 
